b1b0eb4a8f8ace1bb9d2ea87c4e003b8c59f09c3
David H. Angarita <davidangarita.sistemas@gmail.com>
2021-12-28 17:32:13 -0500

    Archivos de arranque

---
+++ src/main/java/co/com/sofka/TestNoReactivo/TestNoReactivoApplication.java
@@ -0,0 +1,13 @@
+package co.com.sofka.TestNoReactivo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TestNoReactivoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TestNoReactivoApplication.class, args);
+	}
+
+}
